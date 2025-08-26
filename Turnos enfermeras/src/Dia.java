865236e59da389fc39cd278ac3d1ea8b579b07f4
Joaquinmalarcon <jmunoz.alarcon2004@gmail.com>
2025-08-25 22:55:33 -0400

    Update Dia.java

--- Turnos enfermeras/src/Dia.java
+++ Turnos enfermeras/src/Dia.java
@@ -2,7 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dia {
-  private String diaSemana; //Lunes, Martes...
-  private List<Turno> turnos;
-}
+    private String diaSemana; //Lunes, Martes...
+    private List<Turno> turnos;
 
+    public Dia(String diaSemana){
+        this.diaSemana = diaSemana;
+        this.turnos = new ArrayList<>();
+    }
+
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public List<Turno> getTurnos() {
+        return turnos;
+    }
+
+    public void setTurnos(List<Turno> turnos) {
+        this.turnos = turnos;
+    }
+}
